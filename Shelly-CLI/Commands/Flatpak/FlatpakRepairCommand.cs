d69faaf2f2c422a06d3f44e0f1466966da75b7de
VinnyQF <viniqf@gmail.com>
2026-05-12 11:24:30 -0300

    Creating FlatpakRepairCommand.cs file.

---
+++ Shelly-CLI/Commands/Flatpak/FlatpakRepairCommand.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+using PackageManager.Flatpak;
+using Shelly_CLI.Utility;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Shelly_CLI.Commands.Flatpak;
+
+public class FlatpakRepairCommand
+{
+    
+}
